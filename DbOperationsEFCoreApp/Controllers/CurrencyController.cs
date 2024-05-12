391630b82d43af716aee24eef46dcfbfe1143ce9
Mahesh K S <mks.maheshks@outlook.com>
2024-05-12 20:17:42 +0530

    Created CurrencyController

---
+++ DbOperationsEFCoreApp/Controllers/CurrencyController.cs
@@ -0,0 +1,28 @@
+﻿using DbOperationsEFCoreApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationsEFCoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+
+        public CurrencyController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCurrencies() 
+        {
+            //var result = await this.appDbContext.Currencies.ToListAsync();
+            var result = await (from currencies in appDbContext.Currencies
+                         select currencies).ToListAsync();
+            return Ok(result);
+        }
+    }
+}
